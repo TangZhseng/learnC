91b488a010e86f79b51af0c6289367dc2c46bd1b
TangZhseng <395267141@qq.com>
2017-12-30 00:04:34 +0800

    fix algs

--- algs/algs-sort.c
+++ algs/algs-sort.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-void swap(int a,int b){
+void swap(int *a,int *b){
 	int t;
-	a=t;
-	a=b;
-	b=t;
+	t=*a;
+	*a=*b;
+	*b=t;
 }
 void bubble_sort(int a[],int ac){//ц╟ещеепР 
 	int i,j;
@@ -20,4 +20,27 @@ void bubble_sort(int a[],int ac){//ц╟ещеепР
 			break:
 		}
 	}
-} 
+}
+
+void insert_sort(int a[],int ac){
+	int i,j;
+	for(i=1;i<ac;i++){
+		j=i
+		while(j>=1&&a[j]<a[j-1]){
+			swap(a[j],a[j-1]);
+			j--;
+		}
+	}
+}
+
+void select_sort(int a[],int ac){
+	int i,j;
+	int sign;
+	for(i=0;i<ac-1;i++){
+		for(j=i+1;j<ac;j++){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]);
+			}
+		}
+	}
+}
