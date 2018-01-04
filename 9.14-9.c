28cb49bcc7b118df68f2e9efe3ac7ab80930473b
TangZhseng <395267141@qq.com>
2018-01-04 17:19:22 +0800

    Update 9.14-9.c

--- 9.14-9.c
+++ 9.14-9.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-//9.14 programming work
+//9.14 programming work //编写一个函数，在第一个参数中查找，并返回匹配能在第二个参数中出现的字符的数量。
 int count_chars(char const *str,char const*chars){
 	int i=0;
 	const char *ip=str,*op=chars;
